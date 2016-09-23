6969f47568bc62efa491555a62d8491446c33d2a
David Hyun Lee <hyun183@Davids-MacBook-Pro.local>
2016-09-22 20:41:20 -0400

    Created initial files

---
+++ assets/javascript/team_feed.js
@@ -0,0 +1,9 @@
+  // https://console.firebase.google.com/project/team-feed/
+  var config = {
+    apiKey: "AIzaSyBEb-mCpF6idbFnd99pdMci9P97LsECPlc",
+    authDomain: "team-feed.firebaseapp.com",
+    databaseURL: "https://team-feed.firebaseio.com",
+    storageBucket: "team-feed.appspot.com",
+    messagingSenderId: "901779519021"
+  };
+  firebase.initializeApp(config);
